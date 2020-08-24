888f2c5f4563e2d308a7b1d32705021d0157ae09
Joon Chung <joonsubtalk@gmail.com>
2020-08-24 01:09:52 -0700

    [initial] upload

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico"/>
+        <meta name="description" content="Free Web tutorials"/>
+        <meta name="keywords" content="HTML, CSS, JavaScript"/>
+        <meta name="author" content="John Doe"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
+      <div>
+        Some Text        
+      </div>
+    </div>
+  )
+}
